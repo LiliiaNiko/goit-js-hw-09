0aae8664072bc68c44f25101cc950bcbd6d9ebe2
LiliiaNiko <96354872+LiliiaNiko@users.noreply.github.com>
2022-06-17 16:11:01 +0000

    Deploying to gh-pages from @ LiliiaNiko/goit-js-hw-09@860c76032083e2c77bd1d773a8878d319e9fbc31 🚀

---
+++ 01-color-switcher.57bb1fc3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(e=>{o=setInterval((()=>{r.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.57bb1fc3.js.map
