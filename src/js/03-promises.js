d8aab290cf84a33fc98d395256f5767a5a98bf1c
LiliiaNikolaichuk <l.nikolaychuk@ukr.net>
2022-06-18 22:45:48 +0300

    Update-task-2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,41 @@
+import Notiflix from 'notiflix';
+
 const delayInput = document.querySelector('input[name="delay"]');
 const stepInput = document.querySelector('input[name="step"]');
 const amountInput = document.querySelector('input[name="amount"]');
-const createBtn = document.querySelector('button');
+const form = document.querySelector('.form');
 
-const onFormSubmit = addEventListener('submit', createPromise);
+
 
 function createPromise(position, delay) {
- 
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+
+    //delay = delayInput + stepInput;
     setTimeout(() => {
       if (shouldResolve) {
         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       }
         reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      }, 500);
+      }, delay);
   })
 }
-for (let i = 0; i < 10; i++) {
-  //position = amountInput.currentTarget.value;
-  //delay = delayInput.currentTarget.value + stepInput.currentTarget.value;
-  createPromise(i)
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-}
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const { elements: { delay, step, amount } } = evt.currentTarget;
+  for (let i = 1; i <= amount.value; i++) {
+         
+    createPromise(i, parseInt(delay.value) + parseInt(step.value))
+      .then((result) => {
+        Notiflix.Notify.success(result);
+        console.log(result)
+  })
+      .catch((error) => {
+        Notiflix.Notify.failure(error);
+        console.log(error)
+      });
+    
+  }
+  
+})
