40290bb1d8c2a20bc16bc9be0fd4aec9fb8af7cb
LiliiaNikolaichuk <l.nikolaychuk@ukr.net>
2022-06-22 16:55:34 +0300

    Update-03-promises-js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,5 @@
 import Notiflix from 'notiflix';
 
-const delayInput = document.querySelector('input[name="delay"]');
-const stepInput = document.querySelector('input[name="step"]');
-const amountInput = document.querySelector('input[name="amount"]');
 const form = document.querySelector('.form');
 
 
@@ -10,8 +7,7 @@ const form = document.querySelector('.form');
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-
-    //delay = delayInput + stepInput;
+  
     setTimeout(() => {
       if (shouldResolve) {
         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -24,18 +20,23 @@ function createPromise(position, delay) {
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const { elements: { delay, step, amount } } = evt.currentTarget;
-  for (let i = 1; i <= amount.value; i++) {
-         
-    createPromise(i, parseInt(delay.value) + parseInt(step.value))
+  
+  for (let i = 1; i <= amount.value; i += 1) {
+    position = i;
+    if (delay.value < 0 || step.value < 0 || amount.value < 0) {
+      return;
+    }
+    createPromise(position, delay.value)
       .then((result) => {
         Notiflix.Notify.success(result);
-        console.log(result)
-  })
+        console.log(result);
+      })
       .catch((error) => {
         Notiflix.Notify.failure(error);
         console.log(error)
       });
-    
-  }
+    delay.valueAsNumber += step.valueAsNumber;
+    }
+    form.reset();
   
-})
+  })
